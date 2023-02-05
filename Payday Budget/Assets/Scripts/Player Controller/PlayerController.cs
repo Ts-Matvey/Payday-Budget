3e696d666a2564d64f54b19ea7abb7363a2cf325
Ts-Matvey <77228909+Ts-Matvey@users.noreply.github.com>
2023-02-05 14:22:07 +0200

    Added player

--- Payday Budget/Assets/Scripts/Player Controller/PlayerController.cs
+++ Payday Budget/Assets/Scripts/Player Controller/PlayerController.cs
@@ -4,15 +4,50 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 10.0f;
+    public float jumpForce = 10.0f;
+    public float jumpDelay = 1.0f;
+
+    private Rigidbody rb;
+    private bool isGrounded;
+    private float lastJumpTime;
+
+    public Transform cameraTransform;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 forward = cameraTransform.forward;
+        Vector3 right = cameraTransform.right;
+
+        forward.y = 0f;
+        right.y = 0f;
+        forward.Normalize();
+        right.Normalize();
+
+        Vector3 movement = forward * vertical + right * horizontal;
+        transform.position += movement * speed * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded && Time.time > lastJumpTime + jumpDelay)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+            lastJumpTime = Time.time;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
     }
 }
